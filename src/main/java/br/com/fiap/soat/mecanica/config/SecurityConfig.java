0b280d4d26e08b74bed607f7955eae9836b8de16
Gabriel Sartoretto <gabrielsartoretto@gmail.com>
2026-07-13 00:19:14 -0300

    subindo kubernetes

--- src/main/java/br/com/fiap/soat/mecanica/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/soat/mecanica/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/auth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/health/**"
                         ).permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
                         .requestMatchers(HttpMethod.GET, "/ordem-servicos/veiculo/placa/**").permitAll()
